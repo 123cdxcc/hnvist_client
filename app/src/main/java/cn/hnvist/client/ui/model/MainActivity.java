f2d8c65dfdd3a009cbd42a63103f52d9f85a9310
muyunming <2569236286@qq.com>
2020-12-09 15:53:34 +0800

    底部导航栏

---
+++ app/src/main/java/cn/hnvist/client/ui/model/MainActivity.java
@@ -0,0 +1,89 @@
+package cn.hnvist.client.ui.model;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import cn.hnvist.client.R;
+import cn.hnvist.client.ui.model.ui.MyActivity;
+import cn.hnvist.client.ui.model.ui.dashboard.DashboardFragment;
+import cn.hnvist.client.ui.model.ui.home.HomeFragment;
+import cn.hnvist.client.ui.model.ui.notifications.NotificationsFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private Fragment homeFragment,dashboardFragment,notificationsFragment,myactivity;
+    private Fragment[] fragments;
+    private int lastIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        initView();
+        getSupportFragmentManager().beginTransaction().add(R.id.nav_host_fragment, homeFragment).show(homeFragment).commit();
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        if (lastIndex != 0){
+                            switchFragment(lastIndex, 0);
+                            lastIndex = 0;
+                        }
+                        return true;
+                    case R.id.navigation_dashboard:
+                        if (lastIndex != 1){
+                            switchFragment(lastIndex, 1);
+                            lastIndex = 1;
+                        }
+                        return true;
+                    case R.id.navigation_notifications:
+                        if (lastIndex != 2){
+                            switchFragment(lastIndex, 2);
+                            lastIndex = 2;
+                        }
+                        return true;
+                    case R.id.id_navigation_wode:
+                        if (lastIndex !=3){
+                            switchFragment(lastIndex,3);
+                            lastIndex=3;
+                        }
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initView() {
+        homeFragment = new HomeFragment();
+        dashboardFragment = new DashboardFragment();
+        notificationsFragment = new NotificationsFragment();
+        myactivity=new MyActivity();
+        fragments = new Fragment[]{homeFragment, dashboardFragment, notificationsFragment,myactivity};
+        lastIndex = 0;
+    }
+
+    private void switchFragment(int lastIndex, int index){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        for (int i = 0; i < 4; i++) {
+            transaction.hide(fragments[i]);
+        }
+        if (!fragments[index].isAdded()){
+            transaction.add(R.id.nav_host_fragment, fragments[index]);
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+    }
+
+}
---
+++ app/src/main/java/cn/hnvist/client/ui/model/ui/MyActivity.java
@@ -0,0 +1,20 @@
+package cn.hnvist.client.ui.model.ui;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.hnvist.client.R;
+
+public class MyActivity extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.activity_my,container,false);
+        return view;
+    }
+}
