9e641dab0170d37d205e9d5a16ecab9c99855e30
PBK-Bin <bin@PBK6.cn>
2020-12-10 19:52:48 +0800

    重命名 MyFragment

--- app/src/main/java/cn/hnvist/client/ui/MainActivity.java
+++ app/src/main/java/cn/hnvist/client/ui/MainActivity.java
@@ -11,14 +11,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import cn.hnvist.client.R;
-import cn.hnvist.client.ui.fragment.MyActivity;
 import cn.hnvist.client.ui.fragment.dashboard.DashboardFragment;
 import cn.hnvist.client.ui.fragment.home.HomeFragment;
 import cn.hnvist.client.ui.fragment.home.tab.news.NewsFragment;
+import cn.hnvist.client.ui.fragment.my.MyFragment;
 import cn.hnvist.client.ui.fragment.notifications.NotificationsFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private Fragment homeFragment,dashboardFragment,notificationsFragment,myactivity;
+    private Fragment homeFragment, dashboardFragment, notificationsFragment, myactivity;
     private Fragment[] fragments;
     private int lastIndex;
 
@@ -32,29 +32,29 @@ public class MainActivity extends AppCompatActivity {
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        if (lastIndex != 0){
+                        if (lastIndex != 0) {
                             switchFragment(lastIndex, 0);
                             lastIndex = 0;
                         }
                         return true;
                     case R.id.navigation_dashboard:
-                        if (lastIndex != 1){
+                        if (lastIndex != 1) {
                             switchFragment(lastIndex, 1);
                             lastIndex = 1;
                         }
                         return true;
                     case R.id.navigation_notifications:
-                        if (lastIndex != 2){
+                        if (lastIndex != 2) {
                             switchFragment(lastIndex, 2);
                             lastIndex = 2;
                         }
                         return true;
                     case R.id.id_navigation_wode:
-                        if (lastIndex !=3){
-                            switchFragment(lastIndex,3);
-                            lastIndex=3;
+                        if (lastIndex != 3) {
+                            switchFragment(lastIndex, 3);
+                            lastIndex = 3;
                         }
                         return true;
                 }
@@ -67,17 +67,17 @@ public class MainActivity extends AppCompatActivity {
         homeFragment = new HomeFragment();
         dashboardFragment = new DashboardFragment();
         notificationsFragment = new NotificationsFragment();
-        myactivity=new MyActivity();
-        fragments = new Fragment[]{homeFragment, dashboardFragment, notificationsFragment,myactivity};
+        myactivity = new MyFragment();
+        fragments = new Fragment[]{homeFragment, dashboardFragment, notificationsFragment, myactivity};
         lastIndex = 0;
     }
 
-    private void switchFragment(int lastIndex, int index){
+    private void switchFragment(int lastIndex, int index) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         for (int i = 0; i < 4; i++) {
             transaction.hide(fragments[i]);
         }
-        if (!fragments[index].isAdded()){
+        if (!fragments[index].isAdded()) {
             transaction.add(R.id.nav_host_fragment, fragments[index]);
         }
         transaction.show(fragments[index]).commitAllowingStateLoss();
--- app/src/main/java/cn/hnvist/client/ui/fragment/my/MyFragment.java
+++ app/src/main/java/cn/hnvist/client/ui/fragment/my/MyFragment.java
@@ -1,4 +1,4 @@
-package cn.hnvist.client.ui.fragment;
+package cn.hnvist.client.ui.fragment.my;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +11,9 @@ import android.view.ViewGroup;
 
 import cn.hnvist.client.R;
 
-public class MyActivity extends Fragment {
+public class MyFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.activity_my,container,false);
+        View view = inflater.inflate(R.layout.fragment_my, container, false);
         return view;
     }
 }
